6345f77c13a29519c7e5b9fda8a1a3a24d5f8315
Glauco Todesco <glauco.todesco@gmail.com>
2021-05-04 14:01:12 -0300

    Fix Date

--- src/main/java/com/facens/poo/system/services/EventService.java
+++ src/main/java/com/facens/poo/system/services/EventService.java
@@ -1,6 +1,5 @@
 package com.facens.poo.system.services;
 
-
 import java.time.LocalDate;
 import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
@@ -19,59 +18,50 @@ import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class EventService {
-    
+
     @Autowired
     private EventRepository repository;
-    
-    public Page<EventDTO> getEvents(PageRequest pageRequest,
-                                    String name,
-                                    String description,
-                                    String place,
-                                    String emailContact,
-                                    LocalDate startDate
-                                    )
-    {
+
+    public Page<EventDTO> getEvents(PageRequest pageRequest, String name, String description, String place,
+            String emailContact, LocalDate startDate) {
         Page<Event> list = repository.find(pageRequest, name, description, place, emailContact, startDate);
         return list.map(e -> new EventDTO(e));
     }
 
-    public EventDTO getEventById(Long id)
-    {
+    public EventDTO getEventById(Long id) {
         Optional<Event> op = repository.findById(id);
         Event event = op.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found by id"));
         return new EventDTO(event);
     }
 
-   
-    public EventDTO insertEvent(EventInsertDTO eventInsertDTO)
-    {
-        Event entity = new Event(eventInsertDTO);
-        entity = repository.save(entity);
-        return new EventDTO(entity);
+    public EventDTO insertEvent(EventInsertDTO eventInsertDTO) {
+
+        if (eventInsertDTO.getStartDate().compareTo(eventInsertDTO.getEndDate()) > 0) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+                    "The end date should be bigger than the start date!");
+        } else {
+            Event entity = new Event(eventInsertDTO);
+            entity = repository.save(entity);
+            return new EventDTO(entity);
+        }
     }
 
-    public void deleteEvent(Long id)
-    {
-        try 
-        {
-            repository.deleteById(id);   
-        } catch (EmptyResultDataAccessException e) 
-        {
+    public void deleteEvent(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found");
         }
     }
 
-    public EventDTO updateEvent(Long id, EventUpdateDTO eventUpdateDTO)
-    {
-        try 
-        {
+    public EventDTO updateEvent(Long id, EventUpdateDTO eventUpdateDTO) {
+        try {
             Event entity = repository.getOne(id);
             entity.setName(eventUpdateDTO.getName());
             entity = repository.save(entity);
             return new EventDTO(entity);
 
-        } catch (EntityNotFoundException e) 
-        {
+        } catch (EntityNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found");
         }
     }
